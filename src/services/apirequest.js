d6ec9b168bcf5b18b69f08fef4d45a9b3b8208b4
akaboxmac <akabox@akabox.akabox>
2022-04-20 22:50:14 +0300

    add apirequest wip

--- src/services/apirequest.js
+++ src/services/apirequest.js
@@ -4,24 +4,47 @@ import axios from "axios";
 
 
 const {REACT_APP_SERVER_URL} = process.env;
+export let apioutput;
 
-export async function apirequest() {
+export function apirequest() {
     console.log("in reqeust");
     let loginRequest;
-    try {
-        loginRequest =await axios.get(
-            `${REACT_APP_SERVER_URL}/apitest`,
-            {}
-        );
-        console.log(loginRequest.data);
+    // try {
+    //     loginRequest = axios.get(
+    //         `${REACT_APP_SERVER_URL}/apitest`,
+    //         {}
+    //     ).then(function (res){
+    //         console.log(loginRequest.data);
+    //         output =JSON.stringify(loginRequest.data);
+    //         return output;
+    //     });
+    //
+    //
+    // } catch ({response}) {
+    //     console.log(response);
+    //     loginRequest = response;
+    //     console.log(loginRequest);
+    // }
 
-    } catch ({response}) {
-        console.log(response);
-        loginRequest = response;
-        console.log(loginRequest);
-    }
+    axios.get(
+        `${REACT_APP_SERVER_URL}/apitest`,
+        {}
+    ).then((response) => {
+        // console.log('Get response data');
+        if (response.data) {
+            // cache.set(to, response.data);
+            // return res.status(200).json(response.data);
+            // console.log(response.data);
+            apioutput = JSON.stringify(response.data);
+            console.log(apioutput);
+            return apioutput;
+        }
+    }).catch((error) => {
+        console.log(error);
+        // return res.json(error);
+    })
 
 
 }
 
-apirequest();
+// apirequest();
