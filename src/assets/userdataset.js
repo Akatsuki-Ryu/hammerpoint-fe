c446c16b5241b09168cd2eadf357b8d682a711c5
akaboxmac <akabox@akabox.akabox>
2022-04-21 21:09:19 +0300

    add profilename for page routing

--- src/assets/userdataset.js
+++ src/assets/userdataset.js
@@ -1,20 +1,25 @@
 export const playerlist = [{
+    "profilename": "akabox",
     "playername": "akabox218",
     "uid": "1007820601979",
     "profilephoto": "https://cdn.discordapp.com/avatars/215068545552351250/5a600bbca2331678b276125998c16ecc.png?size=1024"
 }, {
+    "profilename": "terpko",
     "playername": "terpko",
     "uid": "2297370779",
     "profilephoto": "https://cdn.discordapp.com/avatars/171358872684986368/cc87dcbae8d37e8b5aed2e8da34d4e71.png?size=1024"
 }, {
+    "profilename": "hakipi",
     "playername": "hakipi",
     "uid": "2545398846",
     "profilephoto": "https://cdn.discordapp.com/avatars/284391184393306112/a390ffac93ac94f7a91fbdea0057cc1e.png?size=1024"
 }, {
+    "profilename": "GT",
     "playername": "TheGTRacer97",
     "uid": "2299827182",
     "profilephoto": "https://cdn.discordapp.com/avatars/127868875810406400/ba92a07ff12d732b24c9d7c57478b621.png?size=1024"
 }, {
+    "profilename": "serveri",
     "playername": "The9axel5",
     "uid": "2298814335",
     "profilephoto": "https://cdn.discordapp.com/avatars/222299113713041408/847fbea14b00f3a87385fd7912313948.png?size=1024"
