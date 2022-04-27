b983c6bf539d017c563eaaa84d9f62349dc1efbd
akaboxmac <akabox@akabox.akabox>
2022-04-28 01:28:28 +0300

    add map rotation page

--- src/layouts/maprotation/components/Header/index.js
+++ src/layouts/maprotation/components/Header/index.js
@@ -1,19 +1,19 @@
 /**
-=========================================================
-* Material Dashboard 2 React - v2.1.0
-=========================================================
+ =========================================================
+ * Material Dashboard 2 React - v2.1.0
+ =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2022 Creative Tim (https://www.creative-tim.com)
 
-Coded by www.creative-tim.com
+ Coded by www.creative-tim.com
 
  =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 // prop-types is a library for typechecking of props.
 import PropTypes from "prop-types";
@@ -38,119 +38,129 @@ import breakpoints from "assets/theme/base/breakpoints";
 import burceMars from "assets/images/bruce-mars.jpg";
 import backgroundImage from "assets/images/bg-profile.jpeg";
 
-function Header({ children }) {
-  const [tabsOrientation, setTabsOrientation] = useState("horizontal");
-  const [tabValue, setTabValue] = useState(0);
+function Maprotationheader({children,mapaddr}) {
+    try {
+        mapaddr === "1";
 
-  useEffect(() => {
-    // A function that sets the orientation state of the tabs.
-    function handleTabsOrientation() {
-      return window.innerWidth < breakpoints.values.sm
-        ? setTabsOrientation("vertical")
-        : setTabsOrientation("horizontal");
+    }catch (e) {
+        return "no data";
     }
+    const [tabsOrientation, setTabsOrientation] = useState("horizontal");
+    const [tabValue, setTabValue] = useState(0);
 
-    /** 
-     The event listener that's calling the handleTabsOrientation function when resizing the window.
-    */
-    window.addEventListener("resize", handleTabsOrientation);
+    useEffect(() => {
+        // A function that sets the orientation state of the tabs.
+        function handleTabsOrientation() {
+            return window.innerWidth < breakpoints.values.sm
+                ? setTabsOrientation("vertical")
+                : setTabsOrientation("horizontal");
+        }
 
-    // Call the handleTabsOrientation function to set the state with the initial value.
-    handleTabsOrientation();
+        /**
+         The event listener that's calling the handleTabsOrientation function when resizing the window.
+         */
+        window.addEventListener("resize", handleTabsOrientation);
 
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleTabsOrientation);
-  }, [tabsOrientation]);
+        // Call the handleTabsOrientation function to set the state with the initial value.
+        handleTabsOrientation();
 
-  const handleSetTabValue = (event, newValue) => setTabValue(newValue);
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleTabsOrientation);
+    }, [tabsOrientation]);
 
-  return (
-    <MDBox position="relative" mb={5}>
-      <MDBox
-        display="flex"
-        alignItems="center"
-        position="relative"
-        minHeight="18.75rem"
-        borderRadius="xl"
-        sx={{
-          backgroundImage: ({ functions: { rgba, linearGradient }, palette: { gradients } }) =>
-            `${linearGradient(
-              rgba(gradients.info.main, 0.6),
-              rgba(gradients.info.state, 0.6)
-            )}, url(${backgroundImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "50%",
-          overflow: "hidden",
-        }}
-      />
-      <Card
-        sx={{
-          position: "relative",
-          mt: -8,
-          mx: 3,
-          py: 2,
-          px: 2,
-        }}
-      >
-        <Grid container spacing={3} alignItems="center">
-          <Grid item>
-            <MDAvatar src={burceMars} alt="profile-image" size="xl" shadow="sm" />
-          </Grid>
-          <Grid item>
-            <MDBox height="100%" mt={0.5} lineHeight={1}>
-              <MDTypography variant="h5" fontWeight="medium">
-                Richard Davis
-              </MDTypography>
-              <MDTypography variant="button" color="text" fontWeight="regular">
-                CEO / Co-Founder
-              </MDTypography>
+    const handleSetTabValue = (event, newValue) => setTabValue(newValue);
+
+    return (
+        <MDBox position="relative" mb={5} >
+            <MDBox
+                display="flex"
+                alignItems="center"
+                position="relative"
+                minHeight="28.75rem"
+                borderRadius="xl"
+                sx={{
+                    backgroundImage: ({functions: {rgba, linearGradient}, palette: {gradients}}) =>
+                        `${linearGradient(
+                            rgba(gradients.info.main, 0),
+                            rgba(gradients.info.state,0.1)
+                        )}, url(${mapaddr})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "50%",
+                    overflow: "hidden",
+                }}
+
+            />
+            <MDBox
+                sx={{
+                    position: "relative",
+                    mt: -18,
+                    mx: 3,
+                    py: 2,
+                    px: 2,
+                }}
+
+            >
+                {/*<Grid container spacing={3} alignItems="center">*/}
+                {/*    <Grid item>*/}
+                {/*        <MDAvatar src={burceMars} alt="profile-image" size="xl" shadow="sm"/>*/}
+                {/*    </Grid>*/}
+                {/*    <Grid item>*/}
+                {/*        <MDBox height="100%" mt={0.5} lineHeight={1}>*/}
+                {/*            <MDTypography variant="h5" fontWeight="medium">*/}
+                {/*                Richard Davis*/}
+                {/*            </MDTypography>*/}
+                {/*            <MDTypography variant="button" color="text" fontWeight="regular">*/}
+                {/*                CEO / Co-Founder*/}
+                {/*            </MDTypography>*/}
+                {/*        </MDBox>*/}
+                {/*    </Grid>*/}
+                {/*    <Grid item xs={12} md={6} lg={4} sx={{ml: "auto"}}>*/}
+                {/*        <AppBar position="static">*/}
+                {/*            <Tabs orientation={tabsOrientation} value={tabValue} onChange={handleSetTabValue}>*/}
+                {/*                <Tab*/}
+                {/*                    label="App"*/}
+                {/*                    icon={*/}
+                {/*                        <Icon fontSize="small" sx={{mt: -0.25}}>*/}
+                {/*                            home*/}
+                {/*                        </Icon>*/}
+                {/*                    }*/}
+                {/*                />*/}
+                {/*                <Tab*/}
+                {/*                    label="Message"*/}
+                {/*                    icon={*/}
+                {/*                        <Icon fontSize="small" sx={{mt: -0.25}}>*/}
+                {/*                            email*/}
+                {/*                        </Icon>*/}
+                {/*                    }*/}
+                {/*                />*/}
+                {/*                <Tab*/}
+                {/*                    label="Settings"*/}
+                {/*                    icon={*/}
+                {/*                        <Icon fontSize="small" sx={{mt: -0.25}}>*/}
+                {/*                            settings*/}
+                {/*                        </Icon>*/}
+                {/*                    }*/}
+                {/*                />*/}
+                {/*            </Tabs>*/}
+                {/*        </AppBar>*/}
+                {/*    </Grid>*/}
+                {/*</Grid>*/}
+                {children}
             </MDBox>
-          </Grid>
-          <Grid item xs={12} md={6} lg={4} sx={{ ml: "auto" }}>
-            <AppBar position="static">
-              <Tabs orientation={tabsOrientation} value={tabValue} onChange={handleSetTabValue}>
-                <Tab
-                  label="App"
-                  icon={
-                    <Icon fontSize="small" sx={{ mt: -0.25 }}>
-                      home
-                    </Icon>
-                  }
-                />
-                <Tab
-                  label="Message"
-                  icon={
-                    <Icon fontSize="small" sx={{ mt: -0.25 }}>
-                      email
-                    </Icon>
-                  }
-                />
-                <Tab
-                  label="Settings"
-                  icon={
-                    <Icon fontSize="small" sx={{ mt: -0.25 }}>
-                      settings
-                    </Icon>
-                  }
-                />
-              </Tabs>
-            </AppBar>
-          </Grid>
-        </Grid>
-        {children}
-      </Card>
-    </MDBox>
-  );
+        </MDBox>
+    );
 }
 
 // Setting default props for the Header
-Header.defaultProps = {
-  children: "",
+Maprotationheader.defaultProps = {
+    children: "",
+
 };
 
 // Typechecking props for the Header
-Header.propTypes = {
-  children: PropTypes.node,
+Maprotationheader.propTypes = {
+    children: PropTypes.node,
+    mapaddr: PropTypes.node,
 };
 
-export default Header;
+export default Maprotationheader;
--- src/layouts/maprotation/index.js
+++ src/layouts/maprotation/index.js
@@ -37,6 +37,7 @@ import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
 // Overview page components
 import Header from "layouts/profile/components/Header";
 import PlatformSettings from "layouts/profile/components/PlatformSettings";
+import Maprotationheader from "./components/Header";
 
 // Data
 import profilesListData from "layouts/profile/data/profilesListData";
@@ -50,56 +51,187 @@ import team1 from "assets/images/team-1.jpg";
 import team2 from "assets/images/team-2.jpg";
 import team3 from "assets/images/team-3.jpg";
 import team4 from "assets/images/team-4.jpg";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Statusentitylist from "../serverstatus/components/statusentitylist";
+import Mapinfocard from "./components/mapinfocard";
 
-function maprotation() {
+function rendermapinfocard(posts, whichmap) {
+    if (whichmap === "current") {
+        try {
+            console.log(posts.current);
+            return (
+                <div>
+                    <Mapinfocard
+                        title="Current Map"
+                        description={posts.current.map}
+                        info={{
+                            start: posts.current.readableDate_start,
+                            end: posts.current.readableDate_end,
+                            duration: posts.current.DurationInMinutes + " minutes",
+                            remaining: posts.current.remainingMins + " minutes",
+                        }}
+                        social={[
+                            {
+                                link: "https://www.facebook.com/",
+                                icon: <FacebookIcon/>,
+                                color: "facebook",
+                            },
+                            {
+                                link: "https://twitter.com/",
+                                icon: <TwitterIcon/>,
+                                color: "twitter",
+                            },
+                            {
+                                link: "https://www.instagram.com/",
+                                icon: <InstagramIcon/>,
+                                color: "instagram",
+                            },
+                        ]}
+                        action={{route: "", tooltip: "Edit Profile"}}
+                        shadow={true}
+                    />
+                </div>
+
+            )
+        } catch (e) {
+            return "No data";
+
+        }
+    } else if (whichmap === "next") {
+        try {
+            console.log(posts.current);
+            return (
+                <div>
+                    <Mapinfocard
+                        title="Next Map"
+                        description={posts.next.map}
+                        info={{
+                            start: posts.next.readableDate_start,
+                            end: posts.next.readableDate_end,
+                            duration: posts.next.DurationInMinutes + " minutes",
+                        }}
+                        social={[
+                            {
+                                link: "https://www.facebook.com/",
+                                icon: <FacebookIcon/>,
+                                color: "facebook",
+                            },
+                            {
+                                link: "https://twitter.com/",
+                                icon: <TwitterIcon/>,
+                                color: "twitter",
+                            },
+                            {
+                                link: "https://www.instagram.com/",
+                                icon: <InstagramIcon/>,
+                                color: "instagram",
+                            },
+                        ]}
+                        action={{route: "", tooltip: "Edit Profile"}}
+                        shadow={true}
+                    />
+                </div>
+
+            )
+        } catch (e) {
+            return "No data";
+
+        }
+    } else {
+        return "no data";
+    }
+
+}
+
+
+function Maprotation() {
+
+    const {REACT_APP_SERVER_URL} = process.env;
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        function loadPost() {
+
+            // Till the data is fetch using API
+            // the Loading page will show.
+            setLoading(true);
+
+            axios.get(
+                `${REACT_APP_SERVER_URL}/getmaprotation`,
+                {}
+            ).then((response) => {
+                // console.log('Get response data');
+                // console.log(response.data);
+                if (response.data) {
+                    // cache.set(to, response.data);
+                    // return res.status(200).json(response.data);
+                    // console.log(response.data);
+                    // apioutput = JSON.stringify(response.data);
+                    // console.log(apioutput);
+                    setPosts(response.data);
+
+
+                }
+            }).catch((error) => {
+                console.log(error);
+                // return res.json(error);
+            })
+
+
+            // After fetching data stored it in posts state.
+            // setPosts(response.data);
+
+            // Closed the loading page
+            setLoading(false);
+        }
+
+        // Call the function
+        loadPost();
+    }, []);
+
+    console.log(posts);
+    console.log(posts !== []);
     return (
         <DashboardLayout>
             <DashboardNavbar/>
             <MDBox mb={2}/>
-            {/*<Header>*/}
-                {/*<MDBox mt={5} mb={3}>*/}
-                {/*    <Grid container spacing={1}>*/}
-                {/*        <Grid item xs={12} md={6} xl={4}>*/}
-                {/*            <PlatformSettings/>*/}
-                {/*        </Grid>*/}
-                {/*        <Grid item xs={12} md={6} xl={4} sx={{display: "flex"}}>*/}
-                {/*            <Divider orientation="vertical" sx={{ml: -2, mr: 1}}/>*/}
-                {/*            <ProfileInfoCard*/}
-                {/*                title="profile information"*/}
-                {/*                description="Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."*/}
-                {/*                info={{*/}
-                {/*                    fullName: "Alec M. Thompson",*/}
-                {/*                    mobile: "(44) 123 1234 123",*/}
-                {/*                    email: "alecthompson@mail.com",*/}
-                {/*                    location: "USA",*/}
-                {/*                }}*/}
-                {/*                social={[*/}
-                {/*                    {*/}
-                {/*                        link: "https://www.facebook.com/CreativeTim/",*/}
-                {/*                        icon: <FacebookIcon/>,*/}
-                {/*                        color: "facebook",*/}
-                {/*                    },*/}
-                {/*                    {*/}
-                {/*                        link: "https://twitter.com/creativetim",*/}
-                {/*                        icon: <TwitterIcon/>,*/}
-                {/*                        color: "twitter",*/}
-                {/*                    },*/}
-                {/*                    {*/}
-                {/*                        link: "https://www.instagram.com/creativetimofficial/",*/}
-                {/*                        icon: <InstagramIcon/>,*/}
-                {/*                        color: "instagram",*/}
-                {/*                    },*/}
-                {/*                ]}*/}
-                {/*                action={{route: "", tooltip: "Edit Profile"}}*/}
-                {/*                shadow={false}*/}
-                {/*            />*/}
-                {/*            <Divider orientation="vertical" sx={{mx: 0}}/>*/}
-                {/*        </Grid>*/}
-                {/*        <Grid item xs={12} xl={4}>*/}
-                {/*            <ProfilesList title="conversations" profiles={profilesListData} shadow={false}/>*/}
-                {/*        </Grid>*/}
+            <Maprotationheader mapaddr={posts.current?posts.current.asset:0} >
+                {/*{posts !== [] ? JSON.stringify(posts) : ""}*/}
+                {/*<Grid container spacing={3}>*/}
+                {/*    <Grid item xs={12} lg={4}>*/}
+                {/*        <Statusentitylist serverdata={posts} contentswitch="Server"/>*/}
                 {/*    </Grid>*/}
-                {/*</MDBox>*/}
+                {/*    <Grid item xs={12} lg={4}>*/}
+                {/*        <Statusentitylist serverdata={posts} contentswitch="Selfcore"/>*/}
+                {/*    </Grid>*/}
+                {/*</Grid>*/}
+
+                <MDBox mt={5} mb={3}>
+                    <Grid container spacing={1}>
+
+                        {!loading ?
+                                <Grid item xs={12} md={6} lg={4}>
+                                    {/*<Divider orientation="vertical" sx={{ml: -2, mr: 1}}/>*/}
+                                    {rendermapinfocard(posts, "current")}
+                                    {/*<Divider orientation="vertical" sx={{mx: 0}}/>*/}
+                                </Grid>
+
+                            : ""}
+                        {!loading ?
+                            <Grid item xs={12} md={6} lg={4}>
+                                {/*<Divider orientation="vertical" sx={{ml: -2, mr: 1}}/>*/}
+                                {rendermapinfocard(posts, "next")}
+                                {/*<Divider orientation="vertical" sx={{mx: 0}}/>*/}
+                            </Grid>
+
+                            : ""}
+                        {/*<Grid item xs={12} xl={4}>*/}
+                        {/*    <ProfilesList title="conversations" profiles={profilesListData} shadow={false}/>*/}
+                        {/*</Grid>*/}
+                    </Grid>
+                </MDBox>
                 {/*<MDBox pt={2} px={2} lineHeight={1.25}>*/}
                 {/*    <MDTypography variant="h6" fontWeight="medium">*/}
                 {/*        Projects*/}
@@ -194,10 +326,10 @@ function maprotation() {
                 {/*        </Grid>*/}
                 {/*    </Grid>*/}
                 {/*</MDBox>*/}
-            {/*</Header>*/}
+            </Maprotationheader>
             <Footer/>
         </DashboardLayout>
     );
 }
 
-export default maprotation;
+export default Maprotation;
